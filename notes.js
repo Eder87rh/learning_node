f1c80a66e3238e55ea142ad96172169912268c96
Eder Ramirez <documentador1@telecomo.com.mx>
2019-01-25 10:36:21 -0600

    Debuggin node.js applications

--- notes.js
+++ notes.js
@@ -52,6 +52,7 @@ let removeNote = (title) => {
 }
 
 const logNote = (note) => {
+    debugger;
     console.log('----');
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,10 @@
+let person = {
+    name: 'Eder'
+};
+
+person.age = 31;
+debugger;
+person.name = 'Mike';
+
+console.log('​person', person)
+
